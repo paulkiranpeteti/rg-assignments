f4d063d0de3f5a7c9dcb6f2d4c4c6be7f053c2de
petetipaulkiran <petetipaulkiran@gmail.com>
2025-07-20 21:09:32 +0530

    Finished Task-3 from Assignment-2

--- week2/assignment-2/tasks3/readFiles.js
+++ week2/assignment-2/tasks3/readFiles.js
@@ -1,12 +1,15 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require("fs").promises;
+const path = require("path");
 
 async function displayTransactions() {
   try {
-    // TODO: Read 'transactions.json' file using fs.readFile or fs.readFile with async/await
-   
-  } catch (error) {
-    // TODO: Handle errors that occur during file reading or JSON parsing
+    const filePath = path.join(__dirname, "./data/transactions.json");
+    const data = await fs.readFile(filePath, "utf-8");
+    const transactions = JSON.parse(data);
+    console.log(transactions);
+  } 
+  catch (error) {
+    console.log("Failed to Load the Transactions")
   }
 }
 
--- week2/assignment-2/tasks3/server.js
+++ week2/assignment-2/tasks3/server.js
@@ -1,16 +1,20 @@
 const http = require('http');
 
 // TODO 1: In the below line, replace '' with code to import/require transactions data from './data/transactions.json'
-const transactions = '';
+const transactions = require('./data/transactions.json');
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     // TODO 2: Implement response for '/' endpoint with proper HTTP status and 'Content-Type' using res.writeHead()
-
+    
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello PayPal');
   }
   else if (req.url === '/transactions') {
     // TODO 3: Implement response for '/transactions' endpoint with JSON data and proper headers
-
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(transactions, null, 2));
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found');
